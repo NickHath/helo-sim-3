e03c75c1a7f617f3957f7dc80340dc51fa812731
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-27 10:32:08 -0600

    set up main, proxy, all routes, and wrote boilerplate Profile component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Users from './components/Users/Users';
+import Friends from './components/Friends/Friends';
+import Profile from './components/Profile/Profile';
+import Settings from './components/Settings/Settings';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <Route exact path='/' component={ Home } />
-      </HashRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/users' component={ Users } />
+          <Route path='/friends' component={ Friends } />
+          <Route path='/user' component={ Profile } />
+          <Route path='/user/settings' component= { Settings } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Profile.css';
+
+export default class Profile extends Component {
+  render() {
+    return(
+      <h1>Profile Component</h1>
+    );
+  }
+}
