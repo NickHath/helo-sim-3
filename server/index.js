b246125015487ec6fff12bf7e937fb15b48adf90
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-27 10:19:33 -0600

    set up massive

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+massive(process.env.CONNECTION_STRING).then(db => app.set('db', db));
 
 const PORT = 4200;
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
