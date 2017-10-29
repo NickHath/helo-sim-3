4c242846fa9903807c335134c408eb46e3b55256
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-29 13:22:04 -0600

    wrote auth endpt boilerplate

---
+++ server/controllers/auth_controller.js
@@ -0,0 +1,24 @@
+module.exports = {
+  loginUser: function(req, res) {
+  // .then(() => res.status(200).send('Success')
+  // .catch(err => res.status(500).send(err))
+  },
+
+
+  setUser: function(req, res) {
+  // .then(() => res.status(200).send('Success')
+  // .catch(err => res.status(500).send(err))
+  },
+
+
+  authenticateUser: function(req, res) {
+    // .then(() => res.status(200).send('Success')
+    // .catch(err => res.status(500).send(err))
+  },
+
+
+  logoutUser: function(req, res) {
+    // .then(() => res.status(200).send('Success')
+    // .catch(err => res.status(500).send(err))
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,18 +1,31 @@
 require('dotenv').config();
+
+// dependencies
 const express = require('express')
     , bodyParser = require('body-parser')
     , cors = require('cors')
-    , massive = require('massive');
+    , massive = require('massive')
+    , session = require('express-session');
+
+// controllers
+const authController = require('./controllers/auth_controller');
 
 const app = express();
 
+// middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true
-}))
+}));
+
+// auth endponts
+app.get('/api/auth/login', authController.loginUser);
+app.get('/api/auth/setUser', authController.setUser);
+app.get('/api/auth/authenticated', authController.authenticateUser);
+app.post('/api/auth/logout', authController.logoutUser);
 
 massive(process.env.CONNECTION_STRING).then(db => app.set('db', db));
 
