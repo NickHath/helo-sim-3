48a4e52e81a0136576f3f3c9ab3e9146fe9ed34b
Tyler Cook <cooktylerjames@gmail.com>
2017-10-29 22:03:14 -0600

    index

--- db/create_user.sql
+++ db/create_user.sql
@@ -1,2 +1,2 @@
-insert into users(first_name, last_name)
-values ($1, $2)
+insert into users(first_name, last_name, gender, hair, eye, hobby, birth_day, birth_month, birth_year)
+values ($1, $2, $3, $4, $5, $6, $7, $8, $9)
